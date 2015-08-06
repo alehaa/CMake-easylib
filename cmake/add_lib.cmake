83673c5d344f3c1f808f8611a6ae430b18a5d45d
Alexander Haase <alexander.haase@rwth-aachen.de>
2015-08-06 02:14:24 +0200

    Switched license and enhancements
    - Switched license from LGPLv3 to BSD.
    - OBJECT libraries will now be build as static libraries and linked
      into resulting SHARED libraries.

--- cmake/add_lib.cmake
+++ cmake/add_lib.cmake
@@ -1,23 +1,35 @@
-# This file is part of X-ARF tools.
+# This file is part of CMake-easylib.
 #
-# X-ARF tools is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-# more details.
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
 #
-# You should have received a copy of the GNU General Public License and GNU
-# Lesser General Public License along with this program. If not, see
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
 #
-# http://www.gnu.org/licenses/
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 #
 # Copyright (C)
 #   2015 Alexander Haase <alexander.haase@rwth-aachen.de>
+#
+# All rights reserved.
 #
 
 
@@ -28,44 +40,69 @@ function(add_lib TARGET SOURCES)
 	# remove ${TARGET} from ${ARGV} to use ${ARGV} as ${SOURCES}
 	list(REMOVE_AT ARGV 0)
 
-	# if ${ARGV1} is OBJECT, then we should build an object library.
-	set(lib_type "")
+	# get library type
 	if (${ARGV1} STREQUAL "OBJECT")
-		set(lib_type "OBJECT")
 		list(REMOVE_AT ARGV 0)
+		set(lib_type "OBJECT")
 
-		# build object library for static files
-		add_library(${TARGET}_static OBJECT ${ARGV})
-
-		# build object library for shared files
-		add_library(${TARGET}_shared OBJECT ${ARGV})
-		set_target_properties(${TARGET}_shared PROPERTIES
-			POSITION_INDEPENDENT_CODE True
-		)
 	else ()
-		# replace $<TARGET_OBJECT:...> by the names of shared / static target
-		foreach (lib_type STATIC SHARED)
-			string(TOLOWER ${lib_type} lib_type_lower)
+		set(lib_type "LIBRARY")
+	endif()
 
-			set(source_list_${lib_type} "")
-			foreach (source ${ARGV})
-				string(REGEX REPLACE
-					"TARGET_OBJECTS:([^ >]+)" "TARGET_OBJECTS:\\1_${lib_type_lower}"
-					r_source
-					${source}
-				)
-				list(APPEND source_list_${lib_type} ${r_source})
-			endforeach(source)
 
-			# build library for lib_type
-			add_library(${TARGET}_${lib_type_lower} ${lib_type}
-				${source_list_${lib_type}}
+	# Extract expressions out of source list. If there is a equal namked target,
+	# the expression will be copied. If there are static and shared targets for
+	# the expression, the expression will be deleted and the library linked to
+	# the new target.
+	set(sources "")
+	set(link_libs "")
+	foreach (source ${ARGV})
+		string(REGEX MATCH "TARGET_OBJECTS:([^ >]+)" _source ${source})
+
+		# If expression was found, check if there are targets for static and
+		# shared libs.
+		if (NOT "${_source}" STREQUAL "")
+			string(REGEX REPLACE "TARGET_OBJECTS:([^ >]+)" "\\1" tgt ${_source})
+
+			if (TARGET "${tgt}_static" AND TARGET ${tgt}_shared)
+				list(APPEND link_libs ${tgt})
+			else ()
+				list(APPEND sources ${_source})
+			endif()
+		else ()
+			list(APPEND sources ${source})
+		endif ()
+	endforeach(source)
+
+
+	# Create static and shared target for library. If a method is disabled by
+	# CMake, the library type will not be build.
+	foreach (ltype STATIC SHARED)
+		string(TOLOWER ${ltype} _ltype)
+
+		# OBJECT libraried will be build as static libraries
+		if (${lib_type} STREQUAL "OBJECT")
+			set(ltype "STATIC")
+		endif ()
+
+		# add new target
+		add_library(${TARGET}_${_ltype} ${ltype} ${sources})
+		foreach (lib ${link_libs})
+			target_link_libraries(${TARGET}_${_ltype} ${lib}_${_ltype})
+		endforeach ()
+
+		# if type is shared, we have to build with PiC flag
+		if (${lib_type} STREQUAL "OBJECT" AND ${ltype} STREQUAL "SHARED")
+			set_target_properties(${TARGET}_${_ltype} PROPERTIES
+				POSITION_INDEPENDENT_CODE True
 			)
+		endif()
 
-			# set output name to ${TARGET}
-			set_target_properties(${TARGET}_${lib_type_lower} PROPERTIES
+		# set output name to ${TARGET}
+		if (NOT ${lib_type} STREQUAL "OBJECT")
+			set_target_properties(${TARGET}_${_ltype} PROPERTIES
 				OUTPUT_NAME ${TARGET}
 			)
-		endforeach(lib_type)
-	endif ()
+		endif ()
+	endforeach ()
 endfunction(add_lib)
